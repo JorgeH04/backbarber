9e3c9955680020e432b9a82eac870158cece4a40
Jorge <jhessle04@gmail.com>
2023-03-15 14:47:13 -0300

    N

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.set('port', process.env.PORT || 4000);
 
 //connectDB()
 //app.use(cors({origin: 'https://www.parodipablo.site'}));
-//app.use(cors({origin: 'https://barbdos.netlify.app'}));
-app.use(cors());
+app.use(cors({origin: 'https://barbdos.netlify.app'}));
+//app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); // if req.body is undifined need to add this middleware!
 app.use(express.json());  // if req.body is empty need to add this middleware!
 const PORT = process.env.PORT
